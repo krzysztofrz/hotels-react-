1348fb4d3cdc34a513f835522bd8d45420a74e6c
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2022-06-14 23:30:55 +0200

    Add files via upload

---
+++ src/components/Hotels/Hotels.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import Hotel from './Hotel/Hotel'
+
+class Hotels extends Component{
+    render(){
+        return(
+            <div>
+                <div>hotels</div>
+                <Hotel />
+            </div>
+        )
+    }
+}
+
+
+
+export default Hotels;
