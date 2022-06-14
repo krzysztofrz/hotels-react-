2f130bbb9998583694f0c3c2c27c87f1fcc003b1
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2022-06-14 23:32:53 +0200

    setup project

---
+++ src/components/Header/Searchbar/Searchbar.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+function Searchbar() {
+    return(
+        <div>
+            <input  type="text" placeholder="Wyszukaj hotel"/>
+            <button>Szukaj</button>
+
+        </div>
+    )
+}
+
+
+export default Searchbar
